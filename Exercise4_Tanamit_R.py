ddd8046a57e476f52911134cf2a8a9fb010cad07
Tanamit <70133882+Tanamit@users.noreply.github.com>
2021-04-18 13:18:51 +0700

    Sumit Exercise4

---
+++ Exercise4_Tanamit_R.py
@@ -0,0 +1,11 @@
+
+E = "Foundation English"
+G = "General Business"
+C = "Introduction to Computer System"
+P = "Computer Programing"
+
+print("--- Your Score---")
+print(E, ": 80")
+print(G, ": 75")
+print(C, ": 70")
+print(P, ":85")
